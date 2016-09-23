3ee113b1cd39fb3c5aacfab2dc6e314345210f6e
anupsv <anup1538@gmail.com>
2016-09-23 04:24:02 +0000

    Added Diffie–Hellman class with X25519. X448 was not seen in NaCl, hence decided on using of eccsnacks library currently until NaCl implements or will be wrapping a C implementation.

--- The-Noise-Protocol/Functions.py
+++ The-Noise-Protocol/Functions.py
@@ -1,7 +1,5 @@
 from collections import namedtuple
-import libnacl
-from helpers import *
-
+import eccsnacks
 from libnacl import \
     crypto_box_keypair as x25519_keypair, \
     crypto_box_beforenm as x25519_dh, \
@@ -9,10 +7,8 @@ from libnacl import \
     crypto_hash_sha512 as sha512_hash, \
     crypto_generichash as blake2b_hash
 
-from pysodium import \
-    crypto_aead_chacha20poly1305_decrypt as chachapoly_decrypt, \
-    crypto_aead_chacha20poly1305_encrypt as chachapoly_encrypt
 
+KeyPair = namedtuple('KeyPair', ('public_key', 'private_key'))
 
 class Singleton:
 
@@ -28,9 +24,12 @@ class Singleton:
         raise TypeError('This must will be called only on new.')
 
 
-class Hash(Singleton):
+class HASH(Singleton):
     hash_length = None
     block_length = None
+
+    def HASH(self, inputbytes):
+        raise NotImplementedError
 
     def hmac_hash(self, key, data):
         if len(key) < self.block_length:
@@ -43,29 +42,68 @@ class Hash(Singleton):
         return self.HASH(opad + self.HASH(ipad + data))
 
 
-class BLAKE2b(Hash):
+class BLAKE2b(HASH):
     HASHLEN = 64
     BLOCKLEN = 128
     NAME = b'BLAKE2b'
-    hash = blake2b_hash
+    HASH = blake2b_hash
 
-class BLAKE2s(Hash):
-    hash_length = 32
-    block_length = 64
-    NAME = b'BLAKE2s'
+class BLAKE2s(HASH):
+    HASHLEN = 32
+    BLOCKLEN = 64
+    HASH = b'BLAKE2s'
 
-class SHA256(Hash):
-    hash_length = 32
-    block_length = 64
+class SHA256(HASH):
+    HASHLEN = 32
+    BLOCKLEN = 64
     NAME = b'SHA256'
-    hash = sha256_hash
+    HASH = sha256_hash
 
 
-class SHA512(Hash):
-    hash_length = 64
-    block_length = 128
+class SHA512(HASH):
+    HASHLEN = 64
+    BLOCKLEN = 128
     NAME = b'SHA512'
-    hash = sha512_hash
+    HASH = sha512_hash
+
+# Base class for DH(Diffie - Helman), this will be inherited by other cipher functions
+# and these functions will be called from their respective classes.
+
+class DH(Singleton):
+    DHLEN = None
+    NAME = b''
+
+    @classmethod
+    def GENERATE_KEYPAIR(self):
+        # will be overriden by their respective class, else Error.
+        raise NotImplementedError
+
+    @classmethod
+    def DH(self, keypair, public_key):
+        # will be overriden by their respective class calling NaCl function, else Error.
+        raise NotImplementedError
 
 
+# implemetation of X25519 curve. Inherits DH class.
+# Ref : 10.1. The 25519 DH functions
+class X25519(DH):
+    DHLEN = 32
+    NAME = b'25519'
 
+    @classmethod
+    def GENERATE_KEYPAIR(self):
+        return KeyPair(*x25519_keypair())
+
+    @classmethod
+    def DH(self, keypair, public_key):
+        return x25519_dh(public_key, keypair.private_key)
+
+
+# implemetation of X25519 curve. Inherits DH class.
+# Ref : 10.2. The 448 DH functions
+if __name__ == '__main__':
+    class X448(DH):
+        DHLEN = 56
+        NAME = b'448'
+
+        # Will be wrapping eccsnacks for implementation
